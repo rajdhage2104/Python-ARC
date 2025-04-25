4108f956fa928dc5268baca82a5589292d4ca96a
rajdhage2104 <rajdhage2000@gmail.com>
2025-04-25 12:09:07 +0530

    updated code

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 import os
 import socket
+from datetime import datetime
  
 app = Flask(__name__)
  
@@ -8,18 +9,25 @@ app = Flask(__name__)
 def get_pod_ip():
     return socket.gethostbyname(socket.gethostname())
  
-# Function to get the node IP (from environment variable or fallbac)
+# Function to get the node IP
 def get_node_ip():
-    return os.environ.get('NODE_IP', 'Not available')
+    return os.environ.get('NODE_IP', 'Node IP not set')
  
 @app.route('/')
-def home():
+def display_ips():
     pod_ip = get_pod_ip()
     node_ip = get_node_ip()
     return jsonify({
-        'message': 'Welcome to the Simple REST API',
         'pod_ip': pod_ip,
-        'node_ip': node_ip
+        'node_ip': node_ip,
+        'message': f'Request served by Pod IP: {pod_ip} on Node IP: {node_ip}'
+    })
+ 
+@app.route('/datetime')
+def get_datetime():
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return jsonify({
+        'datetime': current_time
     })
  
 @app.route('/health')
